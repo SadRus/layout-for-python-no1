703d7be67b15f56af11338c056a4b53da7b37184
SadRus <rystixx@yandex.ru>
2023-01-30 01:13:08 +0300

    Update main.py and template.html

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
+import os
 import pandas as pd
 
 from collections import defaultdict
 from datetime import datetime
+from dotenv import load_dotenv
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-
-template = env.get_template('template.html')
 
 def year_comment_word(year):
     num = year % 100
     if 4 < num < 21:
         return 'лет'
-
     num = num % 10
     if num == 1:
         return 'год'
@@ -25,26 +19,44 @@ def year_comment_word(year):
         return 'года'
     return 'лет'
 
-year_now = datetime.now().year
-year_of_company = datetime(year=1920, month=1, day=1).year
-age_of_company = year_now - year_of_company
+def main():
+    load_dotenv()
 
-wines_df = pd.read_excel('wine.xlsx', keep_default_na=False)
-wines_categories = wines_df['Категория'].tolist()
-dct_wines_df = wines_df.to_dict('records')
+    file_path = os.getenv('FILEPATH', default='.')
+    file_name = os.getenv('FILENAME', default='wine.xlsx')
+    full_path = os.path.join(file_path, file_name)
 
-wines_data = defaultdict(list)
-for i in range(len(wines_categories)):
-    wines_data[wines_categories[i]].append(dct_wines_df[i])
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
 
-rendered_page = template.render(
-    wines_data = wines_data,
-    year = age_of_company,
-    year_word = year_comment_word(age_of_company)
-)
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    year_now = datetime.now().year
+    year_of_company = 1920
+    age_of_company = year_now - year_of_company
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    wines_df = pd.read_excel(full_path, keep_default_na=False)
+    wines_categories = wines_df['Категория'].tolist()
+    wines = wines_df.to_dict('records')
+
+    wines_by_category = defaultdict(list)
+    for wine_number, category in enumerate(wines_categories):
+        wines_by_category[category].append(wines[wine_number])
+
+    rendered_page = template.render(
+        wines_by_category = wines_by_category,
+        year = age_of_company,
+        year_word = year_comment_word(age_of_company)
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
